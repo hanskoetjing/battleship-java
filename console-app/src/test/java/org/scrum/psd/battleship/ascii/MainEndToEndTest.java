c567f44579c050780301c809e28872793c9c1a26
Idzi <faridzif@xl.co.id>
2019-09-24 15:05:45 +0700

    Unit test for vertical diagonal

--- console-app/src/test/java/org/scrum/psd/battleship/ascii/MainEndToEndTest.java
+++ console-app/src/test/java/org/scrum/psd/battleship/ascii/MainEndToEndTest.java
@@ -5,8 +5,11 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.scrum.psd.battleship.controller.GameController;
 import org.scrum.psd.battleship.controller.dto.Position;
+import org.scrum.psd.battleship.controller.dto.Ship;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
@@ -43,7 +46,7 @@ public class MainEndToEndTest {
 
     @Test
     public void testInvalidPositionSetting() {
-        Position testPosition = Main.parsePosition("Z1");
+        Position testPosition = Main.parsePosition("Z30");
 
         Assert.assertFalse(testPosition.isValid());
     }
@@ -54,4 +57,41 @@ public class MainEndToEndTest {
 
         Assert.assertTrue(testPosition.isValid());
     }
+
+    @Test
+    public void testPositivePositionHorizontal() {
+        try {
+            gameInput.provideLines("c1", "d1", "e1", "f1", "g1", "b1", "b2", "b3", "b4", "c1", "c2", "c3", "d1", "d2", "d3", "e1", "e2", "e4");
+
+            Main.main(new String[]{});
+        } catch(NoSuchElementException e) {
+            Assert.assertTrue(systemOutRule.getLog().contains("Welcome to Battleship"));
+        }
+
+    }
+
+    @Test
+    public void testPositivePositionVertical() {
+        try {
+            gameInput.provideLines("c1", "c2", "c3", "c4", "c5", "b1", "b2", "b3", "b4", "c1", "c2", "c3", "d1", "d2", "d3", "e1", "e2", "e4");
+
+            Main.main(new String[]{});
+        } catch(NoSuchElementException e) {
+            Assert.assertTrue(systemOutRule.getLog().contains("Welcome to Battleship"));
+        }
+    }
+
+    @Test
+    public void testDiagonal() {
+        try {
+            gameInput.provideLines("f1", "e2", "d3", "c4", "b5", "b1", "b2", "b3", "b4", "c1", "c2", "c3", "d1", "d2", "d3", "e1", "e2", "e4");
+
+            Main.main(new String[]{});
+        }
+        catch(NoSuchElementException e) {
+            Assert.assertTrue(systemOutRule.getLog().contains("Coordinate e2 is not valid, Please Insert Another Coordinate..!!"));
+        }
+
+    }
+
 }
